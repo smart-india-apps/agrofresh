0cb5a8381f5bcd8ee163cee7bc73c2fe43a8f907
bmonish <monishmonib@gmail.com>
2020-12-07 21:50:43 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <h1>Agro Fresh</h1>
+    </div>
+  );
 }
 
 export default App;
