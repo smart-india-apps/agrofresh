6566ec95265f3f66ff719585a54d076458011e2f
bmonish <monishmonib@gmail.com>
2020-12-11 00:38:06 +0530

    Fixes #2

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,7 +2,11 @@ import React from "react";
 import "../css/Signup.css";
 
 function Signup() {
-  return <div className="signup"></div>;
+  return (
+    <div className="signup">
+      <h1>I am signup</h1>
+    </div>
+  );
 }
 
 export default Signup;
